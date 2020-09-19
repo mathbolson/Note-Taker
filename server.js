1f114abb276864b0cdb44124f2430f78426626f0
Matheus Bolson <mathbolson@hotmail.com>
2020-09-18 17:24:19 -0700

    index.js correction

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ const fs = require('fs')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+//set up express app to handle data parsing:
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'))
