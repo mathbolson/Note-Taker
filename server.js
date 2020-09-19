d1e01db416f1e028f7a816f12884109735eeccdb
Matheus Bolson <mathbolson@hotmail.com>
2020-09-18 22:01:55 -0700

    import db.json

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-const express = require("express");
-const path = require("path")
-const fs = require('fs')
-
-const app = express();
-const PORT = process.env.PORT || 3000;
+var express = require("express");
+var path = require("path")
+var fs = require('fs')
+var notes; 
 
 
-//set up express app to handle data parsing:
+var app = express();
+var PORT = process.env.PORT || 3000;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'))
+
+
+var savedNotes = fs.readFileSync("./db/db.json", "UTF-8");
+if (savedNotes) {
+    var oldNotes = JSON.parse(savedNotes);
+    notes = oldNotes;
+} else {
+    notes = [];
+}
