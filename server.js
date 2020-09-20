b5f4166d2f378b3a1a0579f296873945514bd8a8
Matheus Bolson <mathbolson@hotmail.com>
2020-09-19 19:07:01 -0700

    Finishd server.js

--- server.js
+++ server.js
@@ -19,3 +19,56 @@ if (savedNotes) {
 } else {
     notes = [];
 }
+
+
+
+app.get("/api/notes", function(req, res) {
+
+    console.log("bottom one")
+    return res.json(notes);
+  });
+
+app.post("/api/notes", function(req, res) {
+    var newNote = req.body;
+    console.log(newNote);
+    notes.push(newNote);
+    res.json(newNote);
+    assignID();
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes, null, 2), function (err) {
+        if (err) 
+            throw err
+    });
+});
+
+
+app.delete("/api/notes/:id", function (req, res) {
+    console.log(req.params.id);
+    var deleteID = req.params.id;
+    notes.splice(deleteID, 1);
+    assignID();
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes), function (err) {
+        if (err) 
+            throw err
+        });
+    res.json({deletion:"success"})
+});
+
+function assignID() {
+    for (i = 0; i < notes.length; i ++) {
+        notes[i].id = i;
+    }
+}
+
+
+app.get("/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+})
+
+app.get("*", function(req, res){
+    res.sendFile(path.join(__dirname, "public/index.html"));
+})
+
+//starts server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
